7e08d5d41d958f2d0c4de3826e333ef0790d909c
Doug Callaway <doug@orbica.world>
2022-05-29 09:56:00 +0000

    add static geojson

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,60 @@
 import React from 'react';
-import Map, {Marker} from 'react-map-gl';
+import Map, { Source, Layer } from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
 
+const geojson = {
+  type: 'FeatureCollection' as 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature' as 'Feature',
+      properties: {
+        id: 1
+      },
+      geometry: {
+        type: 'Point' as 'Point',
+        coordinates: [-122.4, 37.8]
+      }
+    },
+    {
+      type: 'Feature' as 'Feature',
+      properties: {
+        id: 2
+      },
+      geometry: {
+        type: 'Point' as 'Point',
+        coordinates: [-122.1, 37.7]
+      }
+    }
+  ]
+};
+
+const layerStyle = {
+  id: 'point',
+  type: 'circle' as 'circle',
+  paint: {
+    'circle-radius': 10,
+    'circle-color': '#007cbf'
+  }
+};
+
 function App() {
+  const [viewport, setViewport] = React.useState();
   return (
     <Map
       initialViewState={{
         latitude: 37.8,
         longitude: -122.4,
-        zoom: 14
+        zoom: 9
       }}
       mapLib={maplibregl}
-      style={{width: 800, height: 600}}
+      style={{ width: 800, height: 600 }}
       mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
     >
-      <Marker longitude={-122.4} latitude={37.8} color="red" />
+      <Source id="my-data" type="geojson" data={geojson}>
+        <Layer {...layerStyle} />
+      </Source>
     </Map>
   );
 }
