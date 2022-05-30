9f90bebfad4357de38975e26de6a15c7c6192411
Doug Callaway <doug@orbica.world>
2022-05-30 08:00:23 +0000

    static Norway vehicle locations

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,76 @@ import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
 
-const geojson = {
-  type: 'FeatureCollection' as 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature' as 'Feature',
-      properties: {
-        id: 1
-      },
-      geometry: {
-        type: 'Point' as 'Point',
-        coordinates: [-122.4, 37.8]
-      }
+const data = [
+  {
+    "line": {
+      "lineRef": "SKY:Line:900"
     },
-    {
-      type: 'Feature' as 'Feature',
-      properties: {
-        id: 2
-      },
-      geometry: {
-        type: 'Point' as 'Point',
-        coordinates: [-122.1, 37.7]
-      }
+    "lastUpdated": "2022-05-30T09:09:41+02:00",
+    "location": {
+      "latitude": 60.4175743740052,
+      "longitude": 5.46647113747895
     }
-  ]
-};
+  },
+  {
+    "line": {
+      "lineRef": "SKY:Line:912"
+    },
+    "lastUpdated": "2022-05-30T09:09:42+02:00",
+    "location": {
+      "latitude": 60.7845161575824,
+      "longitude": 5.75279728509486
+    }
+  },
+  {
+    "line": {
+      "lineRef": "SKY:Line:911"
+    },
+    "lastUpdated": "2022-05-30T09:00:49+02:00",
+    "location": {
+      "latitude": 60.586475841701,
+      "longitude": 5.81582332961261
+    }
+  },
+  {
+    "line": {
+      "lineRef": "SKY:Line:911"
+    },
+    "lastUpdated": "2022-05-30T09:09:37+02:00",
+    "location": {
+      "latitude": 60.8293512091041,
+      "longitude": 6.27050469629467
+    }
+  },
+  {
+    "line": {
+      "lineRef": "SKY:Line:915"
+    },
+    "lastUpdated": "2022-05-30T09:09:39+02:00",
+    "location": {
+      "latitude": 60.8139958977699,
+      "longitude": 5.80385321751237
+    }
+  }
+];
+
+const vehiclesGeojson = {
+  type: 'FeatureCollection' as 'FeatureCollection',
+  features: data.map(item => ({
+    type: 'Feature' as 'Feature',
+    properties: {
+      id: item.line.lineRef,
+      lastUpdated: item.lastUpdated
+    },
+    geometry: {
+      type: 'Point' as 'Point',
+      coordinates: [
+        item.location.longitude,
+        item.location.latitude
+      ]
+    }
+  }))
+}
 
 const layerStyle = {
   id: 'point',
@@ -44,15 +89,15 @@ function App() {
   return (
     <Map
       initialViewState={{
-        latitude: 37.8,
-        longitude: -122.4,
-        zoom: 9
+        latitude: 60.5,
+        longitude: 5.8,
+        zoom: 7
       }}
       mapLib={maplibregl}
       style={{ width: 800, height: 600 }}
       mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
     >
-      <Source id="my-data" type="geojson" data={geojson}>
+      <Source id="my-data" type="geojson" data={vehiclesGeojson}>
         <Layer {...layerStyle} />
       </Source>
     </Map>
