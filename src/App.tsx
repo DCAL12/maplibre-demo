0d2f751f86a68365b84b64fab0567a89f61b032a
Doug Callaway <dcal@protonmail.com>
2022-08-11 01:08:00 +0000

    fetch vehcile locations via hasura

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,61 @@
 import React from 'react';
 import Map, { Source, Layer } from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, gql, useQuery } from '@apollo/client';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
 
-const data = [
-  {
-    "line": {
-      "lineRef": "SKY:Line:900"
-    },
-    "lastUpdated": "2022-05-30T09:09:41+02:00",
-    "location": {
-      "latitude": 60.4175743740052,
-      "longitude": 5.46647113747895
-    }
-  },
-  {
-    "line": {
-      "lineRef": "SKY:Line:912"
-    },
-    "lastUpdated": "2022-05-30T09:09:42+02:00",
-    "location": {
-      "latitude": 60.7845161575824,
-      "longitude": 5.75279728509486
-    }
-  },
-  {
-    "line": {
-      "lineRef": "SKY:Line:911"
-    },
-    "lastUpdated": "2022-05-30T09:00:49+02:00",
-    "location": {
-      "latitude": 60.586475841701,
-      "longitude": 5.81582332961261
-    }
-  },
-  {
-    "line": {
-      "lineRef": "SKY:Line:911"
-    },
-    "lastUpdated": "2022-05-30T09:09:37+02:00",
-    "location": {
-      "latitude": 60.8293512091041,
-      "longitude": 6.27050469629467
-    }
-  },
-  {
-    "line": {
-      "lineRef": "SKY:Line:915"
-    },
-    "lastUpdated": "2022-05-30T09:09:39+02:00",
-    "location": {
-      "latitude": 60.8139958977699,
-      "longitude": 5.80385321751237
+const GET_LOCATIONS = gql`
+  query MyQuery {
+    vehicles(codespaceId:"SKY") {
+      line {lineRef}
+      lastUpdated
+      location {
+        latitude
+        longitude
+      }
     }
   }
-];
+`;
 
-const vehiclesGeojson = {
-  type: 'FeatureCollection' as 'FeatureCollection',
-  features: data.map(item => ({
-    type: 'Feature' as 'Feature',
-    properties: {
-      id: item.line.lineRef,
-      lastUpdated: item.lastUpdated
-    },
-    geometry: {
-      type: 'Point' as 'Point',
-      coordinates: [
-        item.location.longitude,
-        item.location.latitude
-      ]
-    }
-  }))
+interface Vehicle {
+  line: any;
+  location: any;
+  lastUpdated: string;
 }
 
-const layerStyle = {
-  id: 'point',
-  type: 'circle' as 'circle',
-  paint: {
-    'circle-radius': 10,
-    'circle-color': '#007cbf'
-  }
-};
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(error)</p>;
 
-function App() {
-  const [viewport, setViewport] = React.useState();
+  const layerStyle = {
+    id: 'point',
+    type: 'circle' as 'circle',
+    paint: {
+      'circle-radius': 10,
+      'circle-color': '#007cbf'
+    }
+  };
+
+  const vehiclesGeojson = {
+    type: 'FeatureCollection' as 'FeatureCollection',
+    features: data.vehicles.map((item: Vehicle) => ({
+      type: 'Feature' as 'Feature',
+      properties: {
+        id: item.line.lineRef,
+        lastUpdated: item.lastUpdated
+      },
+      geometry: {
+        type: 'Point' as 'Point',
+        coordinates: [
+          item.location.longitude,
+          item.location.latitude
+        ]
+      }
+    }))
+  };
+
   return (
     <Map
       initialViewState={{
@@ -101,6 +71,15 @@ function App() {
         <Layer {...layerStyle} />
       </Source>
     </Map>
+  );
+}
+
+function App() {
+  const [viewport, setViewport] = React.useState();
+  return (
+    <div>
+      <DisplayLocations />
+    </div>
   );
 }
 
